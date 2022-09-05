faf7d8af7d1a94ee3a991a18925e0dcdc67283a0
Annesobcn <asm.professeur@gmail.com>
2022-09-05 15:37:02 +0200

    création page accueil: presentation canapés

--- front/js/index.js
+++ front/js/index.js
@@ -1,58 +1,71 @@
-     //Déclaration des variables pour chaque partie de notre section produits 
-     let a = '';
-     let article = '';
-     let h3 = '';
-     let p = '';
-     let img = '';
+     //Recupération des produits éventuellment stockés dans le local storage
+    /* let products = window.localStorage.getItem('products');
 
-     //Récupération de données depuis les donnees json via l'API
-   fetch('http://localhost:3000/api/products')
+     if (products === null) {
+     //Récupération des produits via l'API HTTP
+     const reponse = fetch("http://localhost:3000/api/products");
+     const products = reponse.json();
+     //Transformation des produits en JSON
+     const valeurProduits = JSON.stringify(products);
+     //Stockage des informations dans le localStorage
+     window.localStorage.setItem("products", valeurProducts);
+     }else{
+      products = JSON.parse(products);
+     }
+     */
+    let a = '';
+    let article = ''
+    let img = '';
+    let h3 = '';
+    let p = '';
 
-   .then(function(productsArray) {
-   if(productsArray.ok) {
-      return productsArray.json();
-   }
-   })
-   .then(function(value) {
-      console.log("Connection réussie!");
-      for (i in value){
-      
-  
-   //fonction pour générer l'affichage
-         function genererProduit(i) 
-      {
-      //récupération de l'élément du DOM qui accueillera les fiches 
-      a = document.getElementById("items").appendChild(document.createElement("a"));
-      a.setAttribute("href",  "./product.html?id=" + i + "");
-      a.setAttribute("id", "a_link");
 
-      //création de la fiche produit:éléments qui accueilleront les informations de chaque produit
-            .then function(create) {
-      article = document.createElement("article");
-     a.appendChild(article);
-     img = document.createElement("img");
-     article.appendChild(img);
-     h3 = document.createElement(h3);
-     article.appendChild(h3);
-     p = document.createElement("p");
-     article.appendChild(p);
-           
-   
-     //insertion des informations dans leurs emplacements respectifs
-            .then function(insert) {
-     img.setAttribute("src", value[i], imageUrl);
-     img.setAttribute("alt", value[i], altTxt);
-     h3.setAttribute("id", "productName");
-     h3.innerText = value[i].name;
-     p.setAttribute("id","productDescription");
-     p.innerText = value[i].description;
-   
-            }
-         }
+    fetch("http://localhost:3000/api/products")
+    
+        .then(function openArray(productsArray) {
+          if (productsArray.ok) {
+           return productsArray.json();
+        }
+     })
+      .then(function(products) {     
+          console.log("API loaded");
+          for (i in products){
+     
+            //fonction qui récupère mes information produit pour les fiches de la page d'accueil
+            function genererFiches(i) {
+             //Création des fiches produits      
+         //Récupération de l'élément du DOM qui accueillera les fiches
+          a = document.getElementById("items").appendChild(document.createElement("a"));
+          a.setAttribute("href", "./product.html?id="+ i +"");
+          a.setAttribute("id", "a_link");
+         //récupération de la balise dédiée à un produit Kanap
+         article = document.createElement("article");
+         a.appendChild(article);
+
+         //récupération de l'élément image
+         img = document.createElement("img");
+          //on rattache l'image à produitsElement(la balise article)
+         article.appendChild(img);
+         //accès à l'indice i de la liste produits pour configurer la source de l'image.
+         img.setAttribute("src", products[i].imageUrl);
+         img.setAttribute("alt", products[i].altTxt);
+    
+         //idem pour nom et description du produit
+
+         h3 = document.createElement("h3");
+         article.appendChild(h3);
+         h3.setAttribute("class", "productName");
+         h3.innerText = products[i].name;
+
+         p = document.createElement("p");
+         article.appendChild(p);
+         p.setAttribute("class", "productDescription");
+         p.innerText = products[i].description;  
       }
-   genererProduit(i);
-});
-.catch((erreur) => console.log("erreur"));
+      genererFiches(i);
+    }
+  })
 
-  
-
+  .catch(function(error) {
+    console.log("Api loading : failed!");
+  });
