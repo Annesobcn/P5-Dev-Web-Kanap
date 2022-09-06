acd421c2f51fc1fa81ba61d1684f6280094354be
Annesobcn <asm.professeur@gmail.com>
2022-09-06 16:13:25 +0200

    création fonctions pour générer les informations relative à l'article choisi dans la fiche produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,19 +1,4 @@
-
-//Recupération des produits via requete fetch
-fetch("http://localhost:3000/api/products")
-//récupération des produits sous forme de tableau
-    .then(function openArray(productsArray) {
-      if (productsArray.ok) {
-       return productsArray.json();
-    }
- })
-  .then(function(products) {     
-      console.log("API loaded");
-      console.log(products[id]);
-/*création d'une fiche produit et incorporation des informations 
-*produit sur l'api
- */
-  //Lien entre un produit de la page d'accueil et la page produit
+//Lien entre un produit de la page d'accueil et la page produit
   let completeUrl = location;
   var url = new URL(completeUrl);
   var searchParams = new URLSearchParams(url.search);
@@ -24,17 +9,49 @@ fetch("http://localhost:3000/api/products")
       alert("id not found!");
   };
 
-
-    function ficheProduit(id) {
-        img = document.createElement("img");
-        document.getElementsByClassName("item__content").appendChild(img);
-        img.setAttribute("src", value[id].imageUrl);
-        img.setAttribute("alt", value[id].altTxt);
-
-      
-        
+//Recupération des produits via requete fetch
+fetch("http://localhost:3000/api/products")
+//récupération des produits sous forme de tableau
+    .then(function openArray(productsArray) {
+      if (productsArray.ok) {
+       return productsArray.json();
     }
-  })
+ })
+  .then(function(products) {
+    console.log("API loaded");
+    console.log(products[id]);
+    for (i in products[id]){
+    /*création d'une fiche produit et incorporation des informations 
+    *produit sur l'api
+     */
+    function ficheProduit(i) 
+        {
+        img = document.createElement("img");
+        document.getElementsByClassName("item__img").appendChild(img);
+        img.setAttribute("src", products[id].imageUrl);
+        img.setAttribute("alt", products[id].altTxt);
+        console.log(products[id].altTxt);
+        productName = document.createElement("productName");
+        productName.innerText = products[id].name;
+        document.getElementById('title').appendChild(productName);
+        productName.setAttribute('title'), products[id].name;
+        price = document.getElementById('price');
+        price.innerText = products[id].price;
+        description = document.getElementById('description');
+        description.innerText = products[id].description;
+      
+        //boucle pour choix des couleurs
+        for (i in products[id].colors) {
+            console.log(products[id].colors[i]);
+            couleur = document.createElement("option");
+            getElementById('colors').add(couleur);
+            couleur.innerText = products[id].colors[i];
+
+        }
+      ficheProduit(id);  
+    }
+}
+})
   .catch(function(error) {
     console.log("Api loading : failed!");
   });
