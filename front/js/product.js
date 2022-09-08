fe44df3f946e134c81b89cc730bcc3caf1ca3b5c
Annesobcn <asm.professeur@gmail.com>
2022-09-08 14:23:46 +0200

    création des éléments de la fiche produit sur produit.js et rattachement des infos correspondantes

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,11 @@
+let newItem = '';
 //Lien entre un produit de la page d'accueil et la page produit
   let completeUrl = location;
   var url = new URL(completeUrl);
   var searchParams = new URLSearchParams(url.search);
   if(searchParams.has('id')) {
-  var id = searchParams.get("id");
-  console.log("Item ID is " + id);
+    var id = searchParams.get("id");
+    console.log("Item ID is " + id);
   }else{
       alert("id not found!");
   };
@@ -19,40 +20,56 @@ fetch("http://localhost:3000/api/products")
  })
   .then(function(products) {
     console.log("API loaded");
-    console.log(products[id]);
-    for (i in products[id]){
-    /*création d'une fiche produit et incorporation des informations 
-    *produit sur l'api
-     */
-    function ficheProduit(i) 
-        {
-        img = document.createElement("img");
-        document.getElementsByClassName("item__img").appendChild(img);
-        img.setAttribute("src", products[id].imageUrl);
-        img.setAttribute("alt", products[id].altTxt);
-        console.log(products[id].altTxt);
-        productName = document.createElement("productName");
-        productName.innerText = products[id].name;
-        document.getElementById('title').appendChild(productName);
-        productName.setAttribute('title'), products[id].name;
-        price = document.getElementById('price');
+    console.log(products[id]);//récupération des informations produits
+  
+ 
+      
+      function ficheProduit(products, id) {
+      //Récupération de l'élément du DOM qui accueillera les fiches
+        sectionFiche = document.querySelector(".item");
+      
+        //création élément image et insertion de l'image et de son text alt
+        imageProduit = document.createElement("img");
+       sectionFiche.appendChild(imageProduit);
+       document.querySelector(".item__img").appendChild(imageProduit);
+        imageProduit.setAttribute("src", products[id].imageUrl);
+        imageProduit.setAttribute("alt", products[id].altTxt);
+        
+      //Création balise qui contiendra les informations du produit
+       // infosProduit = document.querySelector(".item__content").appendChild("infosProduit");
+        //infosProduit.dataset.id = products[id].id;
+  //création balise h1 pour titre et insertion du nom de l'article
+        h1 = document.createElement("h1");
+        document.querySelector("#title").appendChild(h1);
+        h1.innerText = products[id].name;
+        h1.setAttribute("id", "product name");
+
+        price = document.querySelector("#price");
         price.innerText = products[id].price;
-        description = document.getElementById('description');
+        description = document.querySelector("#description");
         description.innerText = products[id].description;
       
-        //boucle pour choix des couleurs
-        for (i in products[id].colors) {
-            console.log(products[id].colors[i]);
-            couleur = document.createElement("option");
-            getElementById('colors').add(couleur);
-            couleur.innerText = products[id].colors[i];
+        //fonction pour choix des couleurs
+                
+              function colorSelect(){ 
+                for (var i in products[id].colors)
+                {     
+                  color = document.createElement("option"); 
+                   document.querySelector("#colors").add(color);
+                  //paramétrage de l'élément et insertion des couleurs présentes dans le array colors
+                  color.setAttribute("value", products[id].colors[i]);
+                  color.innerText = products[id].colors[i];
+                  } 
+                 color.appendChild(colorSelect);//ajout de l'élément dans color
 
-        }
-      ficheProduit(id);  
-    }
-}
-})
-  .catch(function(error) {
+              } 
+              colorSelect();
+        }       
+         ficheProduit(products, id); 
+
+ 
+ })
+   .catch(function(error) {
     console.log("Api loading : failed!");
   });
 
