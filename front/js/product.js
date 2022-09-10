2256fd30cb1b4058d59c79e5db729ea45a6a1a81
Annesobcn <asm.professeur@gmail.com>
2022-09-10 19:57:50 +0200

    gestion de l'ajout du produit selectionné sur local storage

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,3 @@
-let newItem = '';
 //Lien entre un produit de la page d'accueil et la page produit
   let completeUrl = location;
   var url = new URL(completeUrl);
@@ -25,24 +24,23 @@ fetch("http://localhost:3000/api/products")
   
     function ficheProduit(products, id) 
       {
-      //Récupération de l'élément du DOM qui accueillera les fiches
+        //Récupération de l'élément du DOM qui accueillera les fiches
         sectionFiche = document.querySelector(".item");
       
         //création élément image et insertion de l'image et de son text alt
         imageProduit = document.createElement("img");
-       sectionFiche.appendChild(imageProduit);
-       document.querySelector(".item__img").appendChild(imageProduit);
+        sectionFiche.appendChild(imageProduit);
+        document.querySelector(".item__img").appendChild(imageProduit);
         imageProduit.setAttribute("src", products[id].imageUrl);
         imageProduit.setAttribute("alt", products[id].altTxt);
         
-      //Création balise qui contiendra les informations du produit
       
-  //création balise h1 pour titre et insertion du nom de l'article
+      
+        //création balises et insertion données pour titre, prix et description
         h1 = document.createElement("h1");
         document.querySelector("#title").appendChild(h1);
         h1.innerText = products[id].name;
         h1.setAttribute("id", "product name");
-
         price = document.querySelector("#price");
         price.innerText = products[id].price;
         description = document.querySelector("#description");
@@ -50,73 +48,129 @@ fetch("http://localhost:3000/api/products")
       
         //fonction pour choix des couleurs             
                             
-        function colorSelect(){ 
+        function colorSelect()
+        { 
           for (i in products[id].colors)
           {     
             color = document.createElement("option"); 
              document.querySelector("#colors").appendChild(color);
-            //paramétrage de l'élément et insertion des couleurs présentes dans le array colors
             color.setAttribute("value", products[id].colors[i]);
-            color.innerText = products[id].colors[i];
-            
-            } 
-          // color.appendChild(option);//ajout de l'élément dans color
-
+            color.innerText = products[id].colors[i];            
+          }          
         } 
         colorSelect();
-  }       
-   ficheProduit(products, id); 
+      }       
+   ficheProduit(products, id);
 
-/*****************************validation produit et envoi au panier ******* */
-function saveProduct(id) {
-  
+//*******localSotrage********/
 
-//récupérer la couleur choisie
-var selectedList = [],
-    selectColors = document.getElementById("colors"),
-  i;
-
-for (i = 0; i < selectColors.length; i++)
+function saveProduct(id) 
 {
-    if (selectColors[i].selected)
-    {
-      selectedList.push(selectColors[i]);
-    }
-}
-
-var choixCouleur = document.getElementById("colors");
-var selectedCouleur = choixCouleur.options[choixCouleur.selectedIndex].value;
-var texteCouleur = choixCouleur.options[choixCouleur.selectedIndex].text;
-
    // récupération des valeurs requises dans le panier: id, couleur et quantité
    const reference = products[id]._id;
-   console.log(reference);
-   const couleur = document.getElementById("colors").value;
-  console.log(couleur);
-   const quantite = document.getElementById("quantity").value;
+   //récupérer le choix de la couleur
+   const choixCouleur = document.getElementById("colors");
+   const couleur = choixCouleur.value;
+   //pour récupérer le choix de la quantité
+   const choixQte = document.querySelector("input");
+   const quantite = choixQte.value;
+   //*ajout des valeurs du produit choisi pour le local storage*/
+  
+   const optionsProduitJson = 
+   {
+    ref: reference,
+    couleur: couleur,
+    quantite: quantite,
+    };
 
-//constante pour faciliter la récupération des données du panier
-  // const panier = JSON.parse(localStorage.getItem("choixProduit"));
-   //condition 1: si pdt absent du panier: on l'ajoute
-   /*if(panier == null) {
-    panier = [] ;
-   }*/
 
-   //ajout des valeurs du produit choisi dans le local storage
-  document.getElementById("addToCart").onclick = () =>{
-            const choixProduitJson = {
-                  ref: reference,
-                  couleur: couleur,
-                  quantite: quantite,
-            }
+    const nouveauProduitDansLocalStorage = JSON.stringify(optionsProduitJson);
+    localStorage.setItem("addToCart", nouveauProduitDansLocalStorage);
 
-            const choixProduit = JSON.stringify(choixProduitJson);
-      localStorage.setItem("addToCart", choixProduit);
-      
-   }
+//constante pour convertir les objets du local storage en objet javascript
+   const produitsDuLocalStorage = JSON.parse(localStorage.getItem("addToCart"));
+   
+    console.log(produitsDuLocalStorage);
+// S'il y a le même produit (même id et même couleur) enregistrés dans le panier
+          if(produitsDuLocalStorage == null){
+           produitsDuLocalStorage = [];
+           produitsDuLocalStorage.push();
+                  console.log(produitsDuLocalStorage);
+          }
+          else
+          {
+         
+                 if(nouveauProduitDansLocalStorage.couleur == Object.couleur && nouveauProduitDansLocalStorage.ref == Object.ref ){
+                  
+                  }
+                else{
+
+                  }
+ 
+          }
+
+
+
+   
+   //condition 1: si panier vide: on ajoute le pdt
+  // if(panier == null) 
+  // {
+    //panier = [] ;
+   //}
+   
+   
+   /* panier.keys(localStorage).forEach(function (keys, i)  
+   {
+    let key = JSON.parse(localStorage.getItem(keys))
+     //si pdt déjà ds panier(même ref + même couleur): ajouter quantité +1 au produit
+    if(key.couleur == choixProduitJson.couleur && key.ref == choixProduitJson.ref)
+    {
+      key.quantite = JSON.parse(key.quantite + JSON.parse(key.choixProduitJson.quantite));
+      panier[i] = JSON.stringify(obj);
+      newItem = true;
+      alert("Vous venez d'ajouter " + choixProduitJson.quantite + "canapé de couleur " +choixProduitJson.couleur +" à votre panier");
+      console.log("Total articles panier: " + panier[key.quantite]);
+      localStorage.setItem("totalPanier" , JSON.stringify(panier));
+
+    }
+  });
+    if (newItem !== true)
+    {
+     //mais si pdt absent du panier mais panier  plein: on l'ajoute aussi
+     panier.push(choixProduit);
+     alert("Vous venez d'ajouter le canapé référence: " + choixProduitJson.ref + "en " + choixProduitJson.couleur +"et " + choixProduitJson.quantite + " d'exemplaire(s)");
+     localStorage.setItem("totalPanier", JSON.stringify(panier));
+ 
+    }*/
+
+   
+
 //condition 2: si le produit selectionné existe déjà dans le panier: on incrémente la quantité uniquement
+      /*   
+panier.sort(function (objs, i)
+{
+  obj = JSON.parse(objs);
 
-}
+  if(obj.couleur == choixProduitJson.couleur && obj.ref == choixProduitJson.ref){
+    
+        obj.quantite = JSON.parse(obj.quantite + JSON.parse(obj.choixProduitJson.quantite));
+
+        panier[i] = JSON.stringify(obj);
+        newItem = true;
+        alert("Vous venez d'ajouter " + choixProduitJson.quantite + "canapé de couleur " +choixProduitJson.couleur +" à votre panier");
+        console.log("Total articles panier: " + panier[obj.quantite]);
+        localStorage.setItem("totalPanier" , JSON.stringify(panier));
+  }
+});
+  if (newItem !== true)
+  {
+    panier.push(choixProduit);
+    alert("Vous venez d'ajouter le canapé référence: " + choixProduitJson.ref + "en " + choixProduitJson.couleur +"et " + choixProduitJson.quantite + " d'exemplaire(s)");
+    localStorage.setItem("totalPanier", JSON.stringify(panier));
+
+  }*/
+    
+  }
 //appel de la fonction au click de l'utilisateur pour valider son choix
 
 document.getElementById("addToCart").addEventListener("click", function () 
